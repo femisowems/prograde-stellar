103f708463f6ad60d5f8c91f11f9e25f824a73c7
Femi Sowemimo <145929041+femisowems@users.noreply.github.com>
2026-01-28 20:07:18 -0700

    Add Capacitor config and dependencies
    Introduced capacitor.config.ts for Capacitor setup and added required Capacitor packages to dependencies in package.json. This enables Capacitor integration for native functionality in the project.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'prograde.stellar',
+  appName: 'prograde-stellar',
+  webDir: 'build'
+};
+
+export default config;
